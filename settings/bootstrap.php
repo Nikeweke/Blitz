0cf76368e7fab4564a807257949d33572210f052
nikeweke <jxammet@gmail.com>
2018-01-12 20:05:09 +0200

    new commit

--- settings/bootstrap.php
+++ settings/bootstrap.php
@@ -4,7 +4,7 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 
 require  'vendor/autoload.php';     // подгрузка пакетов
-include  'settings/database.php';  // БД данные
+// include  'settings/database.php';  // БД данные (Можно заремить если БД нет)
 include  'settings/helpers.php';   // Воспомогательные функции
 
 
@@ -19,13 +19,15 @@ $app = new \Slim\App(['settings' => $config]);
 $container = $app->getContainer();
 
 // Устанавливаем подключение
-$container['db'] = function($c){
-     $db = $c['settings']['db'];
-     $pdo = new PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'],  $db['user'], $db['pass']);
-     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-     return $pdo;
-};
+if($db['host']){
+  $container['db'] = function($c){
+       $db = $c['settings']['db'];
+       $pdo = new PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'],  $db['user'], $db['pass']);
+       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+       $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+       return $pdo;
+  };
+}
 
 
 require 'settings/controllers.php';    // регистрация контроллеров
